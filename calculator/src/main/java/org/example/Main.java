fad1477a693f5031d230868cff4511fa9d6738e9
Gitte <gitte.vandevenne@student.uhasselt.be>
2023-03-22 15:39:11 +0100

    functie optellen

--- calculator/src/main/java/org/example/Main.java
+++ calculator/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public void main(int getal, int getal2) {
+
+        System.out.println(telop(getal, getal2));
     }
+
+    public int telop(int eersteGetal, int tweedeGetal){
+        int resultaat;
+        resultaat = eersteGetal + tweedeGetal;
+        return resultaat;
+    }
+
+
 }
